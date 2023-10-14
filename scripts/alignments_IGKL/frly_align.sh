0855f41061b5279e653500ae5ba45b8e9abce811
nebskinner <62945311+nebskinner@users.noreply.github.com>
2023-10-14 10:55:19 -0400

    Add files via upload

---
+++ scripts/alignments_IGKL/frly_align.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=himem
+#SBATCH --cpus-per-task=6
+#SBATCH --mem-per-cpu=75GB
+#SBATCH --time=7-00:00:00
+
+set -e
+
+module purge
+
+ml load GCC/9.3.0
+ml load OpenMPI/4.0.3
+ml load R/4.2.2
+
+R CMD BATCH /home/gdskinnerlab/nes002/serial_timepoints_C110/scripts/alignments_IGKL/frly_align.R > frly_align.Rout
